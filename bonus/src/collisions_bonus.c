61f38fa3e171ca178869c3fbb4722c6c6848802d
rstepani <mrserjy@gmail.com>
2021-09-29 17:29:05 +0300

    norme

--- bonus/src/collisions_bonus.c
+++ bonus/src/collisions_bonus.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   collisions_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sergey <sergey@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 17:23:56 by sergey            #+#    #+#             */
+/*   Updated: 2021/09/29 17:23:56 by sergey           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/so_long_bonus.h"
 
 static int	coordinates_intersect(t_coordinates a, t_coordinates b)
