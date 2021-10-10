079049adda6a2980cd2a224bf02a4856474610dc
Sergy88 <mrserjy@gmail.com>
2021-10-10 16:46:45 +0300

    run at Linux

---
+++ README.md
@@ -0,0 +1,8 @@
+# so_long
+
+###Description
+A basic graphic project for Linux (with some manipulation can be launched at MacOS) on MiniLibX.
+Requires xorg, x11 and zlib.
+
+
+sudo apt-get update && sudo apt-get install xorg libxext-dev zlib1g-dev
--- src/app_controller.c
+++ src/app_controller.c
@@ -21,8 +21,6 @@ int	leave_game(t_dataset *set)
 	mlx_destroy_image(set->rend->mlx, set->rend->exit.img);
 	mlx_destroy_image(set->rend->mlx, set->rend->hero.img);
 	mlx_destroy_window(set->rend->mlx, set->rend->win);
-	mlx_destroy_display(set->rend->mlx);
-	mlx_loop_end(set->rend->mlx);
 	free(set->rend->mlx);
 	ft_lstclear(&(set->game->collectibles), free_env_obj);
 	ft_lstclear(&(set->game->exits), free_env_obj);
